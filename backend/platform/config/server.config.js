c7b5cefde03522044f4ba166eabc8e63c9fac750
Ran Friedlender <ranf216@gmail.com>
2026-06-09 13:11:06 +0300

    Add WEBAPP_PUBLIC_URL configuration for staging environment

--- backend/platform/config/server.config.js
+++ backend/platform/config/server.config.js
@@ -12,6 +12,8 @@ module.exports = {
     "SOCKET_PORT"                           : 8881,
     "SOCKET_PUBLIC_URL"                     : "http://localhost:8881",
 
+    "WEBAPP_PUBLIC_URL"                     : "https://stg.webapp.code4.iuetechnologies.com",
+
     // "ONELINK_SERVER_PUBLIC_URL"             : "http://localhost:8981/",
     // "ONELINK_SERVER_PORT"                   : 8981
 };
